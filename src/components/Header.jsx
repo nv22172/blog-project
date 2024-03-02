54139a96f03a48b215277a9da8d6a5ca766399b8
nv22172 <160468768+nv22172@users.noreply.github.com>
2024-03-02 07:58:19 +0000

    creating header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const NavItem = ({name}) => {
     <a href="/" className="px-4 py-2">
       {name}
       </a>
-    <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] group-hover:opacity-0">/
+    <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/
     </span>
   </li>
   
