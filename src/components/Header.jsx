648b9ca34e0b0fd1068db568481bf77fb9f18e16
nv22172 <160468768+nv22172@users.noreply.github.com>
2024-03-04 08:52:33 +0000

    project not done yet 5 min 3:40

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,33 +13,52 @@ const NavItemsInfo = [
 ]
 
 const NavItem = ({ item }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+const toggleDropdownHandler = () => {
+  setDropdown((curState) => {
+    return !curState;
+  });
+};
+
   return (
                     <li className='relative group'>
                       {item.type === "link" ? (
                       <>
-                      <a href="/" className="px-4 py-2">{item.name}</a>
-                      <span className="text-blue-500 absolute transition-all duration-500 fant-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+                      <a href="/" className="px-4 py-2">
+                        {item.name}
+                        </a>
+                      <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 fant-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
                         /
                       </span>
                       </>
                       ) : (
-                        <>
-                        <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+                        <div className="flex flex-col items-center">
+                        <button 
+                         className="px-4 py-2 flex gap-x-1 items-center"
+                         onClick={toggleDropdownHandler}>
                           <span>{item.name}</span>
                           <MdOutlineKeyboardArrowDown />
-                          </a>
-                        <div className='hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max'>
-                          <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
+                          </button>
+                        <div 
+                        className={`${
+                          dropdown ? "block" : 'hidden'
+                        } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+                        >
+                          <ul className="bg-dark-soft lg:bg-transparent flex flex-col shadow-lg rounded-lg overflow-hidden">
                             {item.items.map((page) => (
                               
-                                <a href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
-                                  {page}
+                                <a 
+                                  href="/" 
+                                  className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
+                                  >
+                                    {page}
                                 </a>
                               
                             ))}
                           </ul>
                         </div>
-                        </>
+                        </div>
                       )}
                       </li>
   );
