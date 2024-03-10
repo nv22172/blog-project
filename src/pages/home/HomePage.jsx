a79c4e94e6435c5de5ebd782f009a0b60a82fe16
nv22172 <160468768+nv22172@users.noreply.github.com>
2024-03-10 08:32:40 +0000

    hero section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import MainLayout from '../../components/MainLayout'
-import Hero from "./container/Hero"
+import React from "react";
+
+import MainLayout from "../../components/MainLayout";
+import Hero from "./container/Hero";
+
 const HomePage = () => {
   return (
     <MainLayout>
-      <Hero/ >
+      <Hero />
     </MainLayout>
-     
-  )
+  );
 };
 
-export default HomePage
+export default HomePage;
